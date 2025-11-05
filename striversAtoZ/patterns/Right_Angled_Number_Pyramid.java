a4193b8c87e7abf0fa866303eb31bee46d977bfe
GUBBALA SAI HARSHITH <harshithgubbala@gmail.com>
2025-11-05 14:04:34 +0530

    added java codes which Ive practiced

---
+++ striversAtoZ/patterns/Right_Angled_Number_Pyramid.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("n:");
+        int n = sc.nextInt();
+        for(int i = 0; i< n; i++){
+            for(int j = 0; j<=i; j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
