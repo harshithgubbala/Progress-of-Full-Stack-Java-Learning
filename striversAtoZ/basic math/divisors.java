fba9e2277a92644ab500f15f8faa0bf0b00f8101
GUBBALA SAI HARSHITH <harshithgubbala@gmail.com>
2025-11-09 00:20:35 +0530

    modification of divisors code by adding optimal function to it

--- striversAtoZ/basic math/divisors.java
+++ striversAtoZ/basic math/divisors.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Collections;
 class divisors {
     static void print_divisors(int n){
         int end_Loop = n/2;
@@ -7,11 +10,26 @@ class divisors {
                 System.out.print(i + " ");
             }
         }
-        
+        System.out.print(n);
+    }
+    static void optimal(int n){
+        ArrayList<Integer> divisorsList = new ArrayList<Integer>();
+        for(int i = 1; i <= Math.sqrt(n); i++){
+            if(n%i==0){
+                divisorsList.add(i);
+                if(i!=n/i) divisorsList.add(n/i);
+            }
+        }
+        Collections.sort(divisorsList);
+        for(int i : divisorsList){
+            System.out.print(i + " ");
+        }
     }
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         int n = Sc.nextInt();
         print_divisors(n);
+        System.out.println();
+        optimal(n);
     }
 }
